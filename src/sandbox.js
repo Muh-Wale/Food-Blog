1f42c5ee4026d892fbff693368332fd51857b373
Muh-Wale <muhammadolawale27@gmail.com>
2023-09-22 19:56:13 +0100

    final commit

---
+++ src/sandbox.js
@@ -0,0 +1,10 @@
+// const head = document.querySelector('.header');
+// const menu = document.querySelector('.show');
+
+// head.addEventListener('click', () => {
+//     if (menu.classList.contains('hidden')) {
+//         menu.classList.remove('hidden');
+//     } else { 
+//         menu.classList.add('hidden');
+//     }    
+// });    
